04790b9ec9150c14a5bc56dc0b063f864466511f
Adam Weider <adamweider98@gmail.com>
2021-03-12 09:23:06 -0600

    smooth

--- scripts/sensor_nav.py
+++ scripts/sensor_nav.py
@@ -5,12 +5,10 @@
 from dataclasses import dataclass
 from enum import IntEnum
 import math
-from typing import Any, Callable, List, Tuple, TypeVar, Union
+from typing import Any, List, Tuple, TypeVar, Union
 
-import more_itertools as mit
 import rospy
-from rospy_util.vector2 import Vector2
-import rospy_util.vector2 as v2
+import rospy_util.mathf as mathf
 from sensor_msgs.msg import LaserScan
 
 from controller import Cmd, Controller, Sub, cmd, sub
@@ -73,7 +71,7 @@ Msg = Union[Scan]
 ### Update ###
 
 TURN_START_DIST: float = 2.0
-TURN_END_DIST: float = 0.7
+TURN_END_DIST: float = 1.0
 
 
 def update(msg: Msg, model: Model) -> Tuple[Model, List[Cmd[Any]]]:
@@ -83,17 +81,25 @@ def update(msg: Msg, model: Model) -> Tuple[Model, List[Cmd[Any]]]:
         return (model, cmd.none)
 
     if isinstance(model, Drive):
-        range_left = max(msg.ranges[60:90], key=lambda r: r.dist)
+        left_max = max(msg.ranges[60:90], key=lambda r: r.dist)
 
-        if range_left.dist > TURN_START_DIST:
+        if left_max.dist > TURN_START_DIST:
             return (Turn(Direction.LEFT), cmd.none)
 
-        range_right = max(msg.ranges[270:300], key=lambda r: r.dist)
+        right_max = max(msg.ranges[270:300], key=lambda r: r.dist)
 
-        if range_right.dist > TURN_START_DIST:
+        if right_max.dist > TURN_START_DIST:
             return (Turn(Direction.RIGHT), cmd.none)
 
-        return (model, [cmd.drive(0.3)])
+        left_min = min(msg.ranges[45:135], key=lambda r: r.dist)
+        right_min = max(msg.ranges[225:315], key=lambda r: r.dist)
+
+        err_ang = ((90 - left_min.dir) / 90) + ((270 - right_min.dir) / 90)
+        vel_ang = mathf.sign(err_ang) * mathf.smoothstep(
+            low=0, high=0.4, amount=abs(err_ang)
+        )
+
+        return (model, [cmd.velocity(angular=vel_ang, linear=0.3)])
 
     vel_ang = (1.0 if model.dir == Direction.LEFT else -1.0) * 0.15 * math.pi
     ranges_side = (
@@ -108,44 +114,6 @@ def update(msg: Msg, model: Model) -> Tuple[Model, List[Cmd[Any]]]:
 
     return (model, [cmd.velocity(angular=vel_ang, linear=0.35)])
 
-    # ranges = [*mit.split_at(msg.ranges, lambda r: r.dist > 0.75)]
-
-    # if len(sanitized := [rs for rs in ranges if rs]) < 2:
-    #     return (model, cmd.none)
-
-    # rejoined = (
-    #     [sanitized[-1] + sanitized[0], *sanitized[1:-1]] if ranges[-1] else sanitized
-    # )
-
-    # (wall_1, _, *_) = sorted(rejoined, key=len)
-
-    # print(points_from_ranges(wall_1))
-
-    # max_right = max(msg.ranges[270:300], key=lambda r: r.dist)
-    # max_left = max(msg.ranges[60:90], key=lambda r: r.dist)
-
-    # if max_right.dist > 2.0:
-    #     print("corner right")
-
-    # if max_left.dist > 2.0:
-    #     print("corner left")
-
-    # vel_ang = 0.3 * min(max_left.dist, 2.5 * math.pi) if max_left.dist > 2.0 else 0.0
-
-    # return (model, [cmd.velocity(angular=vel_ang, linear=0.6)])
-
-
-def ranges_under(dist_min: float, rs: List[Range]) -> List[Range]:
-    return [r for r in rs if r.dist < dist_min]
-
-
-def points_from_ranges(rs: List[Range]) -> List[Vector2]:
-    return [v2.scale(v2.from_angle(r.dir), r.dist) for r in rs]
-
-
-def split(xs: List[T], pred: Callable[[T], bool]) -> List[List[T]]:
-    return [ys for ys in mit.split_at(xs, pred) if ys]
-
 
 ### Subscriptions ###
 
@@ -155,9 +123,6 @@ def subscriptions(_: Model) -> List[Sub[Any, Msg]]:
 
 
 def angled_ranges(scan: LaserScan) -> Msg:
-    # def rad_signed(deg: int) -> float:
-    #     return math.radians(deg - 360 if deg > 180 else deg)
-
     return Scan([Range(deg, dist) for (deg, dist) in enumerate(scan.ranges)])
 
 
