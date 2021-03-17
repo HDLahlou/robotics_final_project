99bac93b0f16d945c032ad57ac0b7dc403638559
Kailin Wu <wuhua@uchicago.edu>
2021-03-17 02:32:41 -0500

    a star mostly done

--- scripts/a_star.py
+++ scripts/a_star.py
@@ -24,7 +24,7 @@ class Cell:
 class AStar:
 
     def __init__(self):
-        self.robot_estimate = Pose()
+        #self.robot_estimate = Pose()
 
         # MAP AND NODES __________
 
@@ -47,19 +47,27 @@ class AStar:
 
         self.goal = Cell() # the player robot. If successor = goal stop search 
 
+        #init the values for goal 
+        self.goal.i = 0 
+        self.goal.j = 0 
+
         # ODOMETRY __________
 
         # subscribe to odom pose 
         
         self.odom_sub = rospy.Subscriber("odom", Odometry, self.update_odometry)
 
+        self.init_map_nodes()
+
         self.find_path() 
 
     def find_path(self):
-        
+        dest_found = False
+        init_node = Cell(0,0, -1, -1, -1, -1, 0, 0,-1)
+        self.open_list.append
         # while open lit is not empty 
         while len(self.open_list) > 0:
-            # q = smallest f
+            # q = node with smallest f
             # https://stackoverflow.com/questions/5320871/in-list-of-dicts-find-min-value-of-a-common-dict-field
             
             q = min(self.open_list, key=lambda x:x.f)
@@ -75,120 +83,155 @@ class AStar:
             # if valid, find the values for the successor 
             # order: north south east and west 
 
-            if (check_north(q)):
+            if (self.check_north(q)):
                 successors[0] = Cell()
                 successors[0].i = q.i - 1 
                 successors[0].j = q.j 
                 successors[0].parent_i = q.i
                 successors[0].parent_j = q.j 
-                successors[0].h = mannhattan_distance(, self.goal)
-                successors[0].g = 
-                successors[0].f = successors[s].g + successors[s].h
+                successors[0].h = self.mannhattan_distance(successor[0], self.goal)
+                index = self.find_index(successors[0].i, successors[0].j)
+                successors[0].g = self.cell_details[index] + 1
+                successors[0].f = successors[0].g + successors[0].h
             else: 
-                succssors[0] = nan #or something lol 
+                succssors[0] = -1  
 
-            if (check_east(q)):
+            if (self.check_east(q)):
                 successors[1] = Cell()
                 successors[1].i = q.i 
                 successors[1].j = q.j + 1 
                 successors[1].parent_i = q.i
                 successors[1].parent_j = q.j 
-                successors[1].h = mannhattan_distance(, self.goal)
-                successors[1].g = 
-                successors[1].f = successors[s].g + successors[s].h
+                successors[1].h = self.mannhattan_distance(successor[1], self.goal)
+                index = self.find_index(successors[1].i, successors[1].j)
+                successors[1].g = self.cell_details[index] + 1
+                successors[1].f = successors[1].g + successors[1].h
             else: 
-                succssors[1] = nan #or something lol
+                succssors[1] = -1 
 
-            if (check_south(q)):
+            if (self.check_south(q)):
                 successors[2] = Cell()
                 successors[2].i = q.i + 1
                 successors[2].j = q.i 
                 successors[2].parent_i = q.i
                 successors[2].parent_j = q.j 
-                successors[2].h = mannhattan_distance(, self.goal)
-                successors[2].g = 
-                successors[2].f = successors[s].g + successors[s].h
+                successors[2].h = self.mannhattan_distance(successor[2], self.goal)
+                index = self.find_index(successors[2].i, successors[2].j)
+                successors[2].g = self.cell_details[index] + 1
+                successors[2].f = successors[2].g + successors[2].h
             else: 
-                succssors[2] = nan #or something lol
+                succssors[2] = -1 
                            
-            if (check_west(q)):
+            if (self.check_west(q)):
                 successors[3] = Cell()
                 successors[3].i = q.i  
                 successors[3].j = q.j - 1 
                 successors[3].parent_i = q.i
                 successors[3].parent_j = q.j 
-                successors[3].h = mannhattan_distance(, self.goal)
-                successors[3].g = 
-                successors[3].f = successors[s].g + successors[s].h
+                successors[3].h = self.mannhattan_distance(successor[4], self.goal)
+                index = self.find_index(successors[3].i, successors[3].j)
+                successors[3].g = self.cell_details[index] + 1
+                successors[3].f = successors[3].g + successors[3].h
             else: 
-                succssors[3] = nan #or something lol
+                succssors[3] = -1 
+            
+            # now, check each successor 
+            for s in successors: 
+                
+                if (not (s == -1)) and self.in_closed_list(s):
+                    # if successor is goal stop search 
+                    if (s.i == self.goal.i and s.j == self.goal.j):
+                        dest_found = True
+                        print("Found Destination")
+                        g_index = self.find_index(self.goal.i, self.goal.j)
+                        self.cell_details[g_index].parent_i = q.i
+                        self.cell_details[g_index].parent_j = q.j
+                        break; 
+
+                    s_index =  self.find_index(s.i, s.j)
+                    if self.cell_details[s_index].f == -1 or self.cell_details[s_index].f > s.f: 
+                        self.open_list.append(s)
+                        self.cell_details[s_index].f = s.f
+                        self.cell_details[s_index].g = s.g
+                        self.cell_details[s_index].h = s.h
+                        self.cell_details[s_index].parent_i = s.parent_i
+                        self.cell_details[s_index].parent_j = s.parent_j
+
+            # push q to the closed list 
+            closed_list.append(q)
+            if dest_found:
+                break
+        print("End of search")
+        self.trace_path()
+
+
+    def in_closed_list(self,cell):
+        for c in self.closed_list:
+            if (c.i == cell.i and c.j == cell.j):
+                    return True
+        return False 
             
 
-            closed_list.append(q)
-
-        # if successor is the goal, stop search
-        if successor 
-
-    def find_index(i,j):
+    def find_index(self,i,j):
         return (i * self.col_num) + j
 
-    def pos_to_map_data_index(y):
+    def pos_to_map_data_index(self,y):
         return int((((y-.5)/self.map.info.resolution) - self.map.info.origin.position.y + self.yrange/2) * self.yrange)
 
     
     # check which directions you can move in 
-    def check_north(parent):
+    def check_north(self,parent):
             if (parent.i == 0):
-                return 0 
+                return False 
             location = pos_to_map_data_index(parent.mapy)
 
             # check a grid space away and see if you run into a wall 
-            for v in range (location, location - xrange * gridsize, -self.xrange):
-                if self.map.data[v] = 100 
-                return 0 #false, you cannot move north            
-            return 1 #true, you can move north 
+            for v in range (location, location - self.xrange * self.gridsize, -self.xrange):
+                if self.map.data[v] > 0:
+                    return False #false, you cannot move north            
+            return True #true, you can move north 
 
-    def check_south():
+    def check_south(self,parent):
             if (parent.i == 12):
-                return 0
+                return False
             
             location = pos_to_map_data_index(parent.mapy)
 
             # check a grid space away and see if you run into a wall 
-            for v in range  (location, location - xrange * gridsize, -self.xrange):
-                if self.map.data[v] = 100
-                return 0 #false, you cannot move north           
-            return 1 #true, you can move north 
+            for v in range  (location, location + self.xrange * self.gridsize, self.xrange):
+                if self.map.data[v] > 0:
+                    return False #false, you cannot move north           
+            return True #true, you can move north 
 
-    def check_east():
+    def check_east(self,parent):
             if (parent.j == 12):
-                return 0
+                return False
             
             location = pos_to_map_data_index(parent.mapy)
 
             # check a grid space away and see if you run into a wall 
-            for v in range  (location, location + gridsize):
-                if self.map.data[v] = 100
-                return 0 #false, you cannot move north            
-            return 1 #true, you can move north 
+            for v in range  (location, location + self.gridsize):
+                if self.map.data[v] > 0:
+                    return False #false, you cannot move north            
+            return True #true, you can move north 
     
-    def check_west(): 
+    def check_west(self,parent): 
             if (parent.j == 0):
-                return 0
+                return False
 
             location = pos_to_map_data_index(parent.mapy)
 
             # check a grid space away and see if you run into a wall 
-            for v in range  (location, location - gridsize):
-                if self.map.data[v] = 100
-                return 0 #false, you cannot move north             
-            return 1 #true, you can move north 
+            for v in range  (location, location - self.gridsize):
+                if self.map.data[v] > 0:
+                    return False #false, you cannot move north             
+            return True #true, you can move north 
 
-    def manhattan_distance(current_cell , goal):
-        return math.abs(current_cell.x - goal.x) + math.abs(current_cell.y - goal.y)
+    def manhattan_distance(self,current_cell , goal):
+        return math.abs(current_cell.i - goal.i) + math.abs(current_cell.j - goal.j)
     
     def init_map_nodes(self):
-        self.open_list = []
+        self.cell_details = []
         xrange = (self.map.info.width - 60) / 13
         yrange = (self.map.info.height - 60) / 13
         
@@ -196,13 +239,39 @@ class AStar:
             for y in range(13):
                 cx = ((x)*xrange - ((13/2)*xrange) + self.map.info.origin.position.x)*self.map.info.resolution +.5
                 cy = ((y)*yrange - ((13/2)*yrange) + self.map.info.origin.position.y)*self.map.info.resolution + .5
-                
-                c = Cell(x, y, cx, cy, -1, -1, -1, -1, -1)
+                f = -1
+                g = -1
+                if (x == self.starting_position.j and y == self.starting_position.i):
+                    f = 0
+                    g = 0
+                c = Cell(x, y, cx, cy, -1, -1, f, g, -1)
+                # self.open_list.append(c)
+                self.cell_details.append(c)
 
-                self.open_list.append(c)
+    # trace the path once the destination has been found 
+    def trace_path(self):
+        print("The path is ")
+        self.path = []
+        row = self.goal.i
+        col = self.goal.j 
 
+        path = [] 
+        
+        while (not ((row == self.starting_position.i) and (col = self.starting_position.j))):
+            index = self.find_index(row, col)
+            cell = self.cell_details[index]
+            path.append(cell)
+            row = cell.parent_i
+            col = cell.parent_j
 
+        start_index = self.find_index(row, col)
+        start_cell = self.cell_details[start_index]
+        path.append(start_cell)
+        
+        for c in path:
+            print(c.i + ", " + c.j)
 
+        self.path = path
 
     def get_map(self, data):
         self.map = data
